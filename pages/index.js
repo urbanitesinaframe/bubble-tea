b67873ddb27324c3977565275a8bdc7407f1cd76
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-15 20:27:18 +0200

    delete all old pics

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head' //neu hinzugefügt
-import { PrismicRichText, SliceZone } from '@prismicio/react'
-import { createClient } from '../prismicio'
-import { components } from '../slices'
-import React, { useState } from 'react';
-
+import Head from "next/head"; //neu hinzugefügt
+import { PrismicRichText, SliceZone } from "@prismicio/react";
+import { createClient } from "../prismicio";
+import { components } from "../slices";
+import React, { useState } from "react";
 
 export default function Page({ page }) {
   const { data } = page;
@@ -14,29 +13,46 @@ export default function Page({ page }) {
   };
 
   return (
-    <><div>
-      <Head>
-        <title>Protest gegen Deutsche Welle</title>
-        <meta property="og:title" content="Protest gegen Deutsche Welle" key="title" />
-      </Head>
-      <Head> 
-        <title>DW-Mitarbeitende protestieren gegen Kürzungen</title>
-        <meta name="description" content="Die Deutsche Welle will bis zu 300 freie und befristet angestellte Mitarbeiter beenden. Diese Menschen sind zentral für die Berichterstattung der Ressorts Sport, Kultur und Nachrichten in deutscher Sprache." />
+    <>
+      <div>
+        <Head>
+          <title>Bubble Tea Shop</title>
+          <meta property="og:title" content="Bubble Tea Shop" key="title" />
+        </Head>
+        <Head>
+          <title>Bubble Tea Shop</title>
+          <meta
+            name="description"
+            content="Dein neuer Lieblings Bubble Tea Shop"
+          />
 
-        <meta property="og:url" content="https://protest-gegen-dw-deutsche-welle.de"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="DW-Mitarbeitende protestieren gegen Kürzungen"/>
-        <meta property="og:description" content="Die Deutsche Welle will bis zu 300 freie und befristet angestellte Mitarbeiter beenden. Diese Menschen sind zentral für die Berichterstattung der Ressorts Sport, Kultur und Nachrichten in deutscher Sprache."/>
-        <meta property="og:image" content="https://images.prismic.io/dwfreie/a52f5032-845a-4f2e-928c-5ff318ba12b0_OpenGraph_DW_Protest.JPG"/>
+          <meta property="og:url" content="Hier kommt die URL hin" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Bubble Tea Shop" />
+          <meta
+            property="og:description"
+            content="Dein neuer Lieblings Bubble Tea Shop"
+          />
+          <meta
+            property="og:image"
+            content="hier kommt der Link zum Bild rein"
+          />
 
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:domain" content="protest-gegen-dw-deutsche-welle.de"/>
-        <meta property="twitter:url" content="https://protest-gegen-dw-deutsche-welle.de"/>
-        <meta name="twitter:title" content="DW-Mitarbeitende protestieren gegen Kürzungen"/>
-        <meta name="twitter:description" content="Die Deutsche Welle will bis zu 300 freie und befristet angestellte Mitarbeiter beenden. Diese Menschen sind zentral für die Berichterstattung der Ressorts Sport, Kultur und Nachrichten in deutscher Sprache."/>
-        <meta name="twitter:image" content="https://images.prismic.io/dwfreie/a52f5032-845a-4f2e-928c-5ff318ba12b0_OpenGraph_DW_Protest.JPG"/>
-      </Head>
-    </div><div>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="Hier kommt die URL hin" />
+          <meta property="twitter:url" content="Hier kommt die URL hin" />
+          <meta
+            name="twitter:title"
+            content="Dein neuer Lieblings Bubble Tea Shop"
+          />
+          <meta
+            name="twitter:description"
+            content="Dein neuer Lieblings Bubble Tea Shop"
+          />
+          <meta name="twitter:image" content="Hier kommt das Bild rein" />
+        </Head>
+      </div>
+      <div>
         <div className="intro">
           <div className="introHeadline">
             <PrismicRichText field={data.headline} />
@@ -53,45 +69,36 @@ export default function Page({ page }) {
           {showPopup && (
             <div className="popup">
               <div className="popupContent">
-                <p>Inhaltlich verantwortlich: <br />Klaus Enderle</p>
-                <p>Kontakt: <br />this-is-us-dw(at)posteo.de</p>
-                <p>Technische Umsetzung: <br />Jimmy Ng</p>
+                <p>
+                  Inhaltlich verantwortlich: <br />
+                  Yvonne Lai
+                </p>
+                <p>
+                  Kontakt: <br />
+                  Wird noch hinzugefügt
+                </p>
+                <p>
+                  Technische Umsetzung: <br />
+                  Jimmy Ng
+                </p>
                 <button onClick={handlePopup}>Close</button>
               </div>
             </div>
           )}
         </div>
-      </div></>
-  )
+      </div>
+    </>
+  );
 }
 
-
 export async function getStaticProps({ previewData }) {
-  const client = createClient({ previewData })
-  const page = await client.getSingle('home')
+  const client = createClient({ previewData });
+  const page = await client.getSingle("home");
 
   return {
     props: {
       page,
     },
     revalidate: 2,
-  }
+  };
 }
-/* //import von https://www.garymeehan.ie/articles/handling-seo-with-nextjs-and-prismic Start
-export const DEFAULT_SEO = {
-  title: 'Wir sind das DW Programm',
-  description: 'Protestaktion gegen die radikale Kürzung der Programmacher:innen',
-  openGraph: {
-    type: 'website',
-    locale: 'en_IE',
-    url: 'https://protest-gegen-dw-deutsche-welle.de',
-    title: 'Wir sind das DW Programm',
-    description: 'Protestaktion gegen die radikale Kürzung der Programmacher:innen',
-    image:
-      'https://images.prismic.io/dwfreie/a52f5032-845a-4f2e-928c-5ff318ba12b0_OpenGraph_DW_Protest.JPG',
-    site_name: 'protest-gegen-dw-deutsche-welle.de',
-    imageWidth: 1200,
-    imageHeight: 1200,
-  }
-}
-//import Ende */
